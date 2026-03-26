9e40664a25838eb14e12a084062fd1ecbef43f01
Jesus200995 <jesus.riosgomez@utxicotepec.edu.mx>
2026-03-27 01:24:02 +0530

    fix: add localhost:5174,5175 to CORS origins

--- backend-python/main.py
+++ backend-python/main.py
@@ -27,6 +27,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
         "http://localhost:4173",
         "https://maiz.geodatos.com.mx",
     ],
